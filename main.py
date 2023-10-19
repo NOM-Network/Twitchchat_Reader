58606d96d9fed2786eeeb56dc45cd6fc41556d8b
KhaledDev <56503740+KhaledDev@users.noreply.github.com>
2023-10-20 00:05:46 +0300

    Changed the channel & added sub/Follow Events
    Sub and follow events do not work due to me having to open a port where its paywalled in my country. Ill contact my ISP tmrw.

--- main.py
+++ main.py
@@ -1,13 +1,31 @@
-from twitchio.ext import commands, pubsub
+import twitchio
+from twitchio.ext import commands, pubsub, eventsub
 import asyncio
 
+
 user_oauth_token = "oauth:hiwnardtvp6dj4pcjplyids2ue6b7p"
+client_secret = "b0tjse2f34u6wezzhxco7z2ozo8d1o"
+user_channel_id = "khaleddev_"
+
+esbot = commands.Bot.from_client_credentials(client_id=user_channel_id,client_secret=client_secret)
+
+esclient = eventsub.EventSubClient(esbot,webhook_secret="",callback_route="")
 
 class Bot(commands.Bot):
     def __init__(self):
         # Initialise our Bot with our access token, prefix and a list of channels to join on boot...
-        super().__init__(token=user_oauth_token, prefix="", initial_channels=["riotgames"])
+        super().__init__(token=user_oauth_token, prefix="", initial_channels=["khaleddev_"])
+        self.pubsub = pubsub.PubSubPool(self)       
+    
+    async def __ainit__(self) -> None:
 
+        self.loop.create_task(esclient.listen(port=1433))
+
+        try:
+            await esclient.subscribe_channel_follows_v2(broadcaster=user_channel_id,moderator=user_channel_id)
+        except twitchio.HTTPException:
+            pass
+    
     async def event_ready(self):
         # We are logged in and ready to chat and use commands...
         print(f'Logged in as | {self.nick}')
@@ -18,6 +36,7 @@ class Bot(commands.Bot):
     #this function prints the chat bot messages.
     async def event_message(self,Message):
         print(f"{Message.author.display_name}: {Message.content}")
+        pass
         
     #This functions prints bits and their messages
     async def event_pubsub_bits(self,event: pubsub.PubSubBitsMessage):
@@ -28,11 +47,16 @@ class Bot(commands.Bot):
         print(event.reward.prompt)
         if str(event.reward.title).lower == "highlight my message":
             print(f"{event.user.name}: {event.reward.prompt}")
-    
-    #this function prints out if a user has subbed.
-    async def event_usernotice_subscription(self, metadata):
-        print(f"{metadata.user.display_name} has subscribed")
-    
 
 bot = Bot()
+bot.loop.run_until_complete(bot.__ainit__())
+
+@esbot.event()
+async def event_eventsub_notification_followV2(payload: eventsub.ChannelFollowData) -> None:
+    print('Received event!')
+    channel = bot.get_channel(user_channel_id)
+    await channel.send(f'{payload.user.name} followed woohoo!')
+
+
+
 bot.run()
