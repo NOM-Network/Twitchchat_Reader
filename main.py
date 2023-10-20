87ad7488d0756aaf5fdcba5efd5ab210037c5814
KhaledDev <56503740+KhaledDev@users.noreply.github.com>
2023-10-20 12:37:52 +0300

    sill trying to get the follow/sub events to work

--- main.py
+++ main.py
@@ -1,62 +1,62 @@
 import twitchio
-from twitchio.ext import commands, pubsub, eventsub
+from twitchio.ext import commands, eventsub
 import asyncio
 
-
+#Add Proirity system for each message
 user_oauth_token = "oauth:hiwnardtvp6dj4pcjplyids2ue6b7p"
-client_secret = "b0tjse2f34u6wezzhxco7z2ozo8d1o"
+bot_token = "chatreader01"
+client_secret = "dh16y4hwzhkjgtseadik9tpu4waruy"
 user_channel_id = "khaleddev_"
 
-esbot = commands.Bot.from_client_credentials(client_id=user_channel_id,client_secret=client_secret)
-
-esclient = eventsub.EventSubClient(esbot,webhook_secret="",callback_route="")
 
 class Bot(commands.Bot):
     def __init__(self):
         # Initialise our Bot with our access token, prefix and a list of channels to join on boot...
         super().__init__(token=user_oauth_token, prefix="", initial_channels=["khaleddev_"])
-        self.pubsub = pubsub.PubSubPool(self)       
-    
-    async def __ainit__(self) -> None:
-
-        self.loop.create_task(esclient.listen(port=1433))
-
-        try:
-            await esclient.subscribe_channel_follows_v2(broadcaster=user_channel_id,moderator=user_channel_id)
-        except twitchio.HTTPException:
-            pass
-    
+        self.esclient = eventsub.EventSubWSClient(self)
+       
     async def event_ready(self):
         # We are logged in and ready to chat and use commands...
         print(f'Logged in as | {self.nick}')
         print(f'User id is | {self.user_id}')
-    
-    #Add Proirity system for each message
-    
+
     #this function prints the chat bot messages.
     async def event_message(self,Message):
-        print(f"{Message.author.display_name}: {Message.content}")
-        pass
-        
-    #This functions prints bits and their messages
-    async def event_pubsub_bits(self,event: pubsub.PubSubBitsMessage):
-        print(f"{event.user.name}: {event.message.content}")
+        print(f"{Message.author.is_mod} {Message.author.display_name}: {Message.content}")
     
-    #This function checks if the redeem is a highlight one then prints it.
-    async def event_pubsub_channel_points(self,event: pubsub.PubSubChannelPointsMessage):
-        print(event.reward.prompt)
-        if str(event.reward.title).lower == "highlight my message":
-            print(f"{event.user.name}: {event.reward.prompt}")
+    async def event_eventsub_notification(self, payload: eventsub.NotificationEvent) -> None:
+        print('Received event!')
+        print(payload.headers.message_id)   
+        
+    async def event_eventsub_notification_channel_reward_redeem(self, payload: eventsub.CustomRewardRedemptionAddUpdateData) -> None:
+        print('Received event!')
+        print(payload.data.id)   
 
+
+    async def event_eventsub_notification_stream_start(self, payload: eventsub.StreamOnlineData) -> None:
+        print('Received event!')
+        print(payload)
+              
+
+    async def event_eventsub_notification_followV2(self, payload: eventsub.ChannelFollowData) -> None:
+        print('Received event!')
+        print(f'{payload.data.user.name} followed woohoo!')
+
+    async def event_eventsub_notification_channel_update(self, payload: eventsub.ChannelUpdateData) -> None:
+        print('Received event!')
+        print(payload)
+    
+    async def event_eventsub_subscribe_channel_bans(self,payload: eventsub.ChannelBanData) -> None:
+        print("Recieved Event")
+        print(payload)
+
+    async def sub(self):
+        await self.esclient.subscribe_channel_points_redeemed(broadcaster=user_channel_id, token=user_oauth_token)
+        await self.esclient.subscribe_channel_stream_start(broadcaster=user_channel_id, token=user_oauth_token)
+        await self.esclient.subscribe_channel_update(broadcaster=user_channel_id, token=user_oauth_token)
+        await self.esclient.subscribe_channel_follows_v2(broadcaster=user_channel_id, moderator=user_channel_id, token=user_oauth_token)
+        await self.esclient.subscribe_channel_bans(broadcaster=user_channel_id,token=user_oauth_token)
+    
 bot = Bot()
-bot.loop.run_until_complete(bot.__ainit__())
-
-@esbot.event()
-async def event_eventsub_notification_followV2(payload: eventsub.ChannelFollowData) -> None:
-    print('Received event!')
-    channel = bot.get_channel(user_channel_id)
-    await channel.send(f'{payload.user.name} followed woohoo!')
-
-
-
+bot.loop.create_task(bot.sub())
 bot.run()
